d39f0e60186353b4294e6e14e484fba7b91075c7
shabiva <zubenyu@gmail.com>
2023-03-09 15:02:51 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let a = +prompt('Enter number first', '');
+let b = +prompt('Enter number second', '');
+let c = +prompt('Enter number third', '');
+let sum =0;
+
+sum = (a + b + c) / 3;
+
+alert(sum);
+
+console.log(sum);
